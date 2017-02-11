43dce16cab4428b272dff580b199e44495ec98aa
SHENOISZ <marcelo.net.system@gmail.com>
2017-02-11 17:19:48 -0300

    run app

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node_modules/.bin/electron main.js "/home/shenoisz/Documents/estudos/node js/electron-project/video/app/media/Ellie Goulding - Love Me Like You Do (Official Video)####.mp4"
+node_modules/.bin/electron main.js "your-video.mp4"
